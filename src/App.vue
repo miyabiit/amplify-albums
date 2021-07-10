3d3f4a9ce9f1aea4b788d400a32d662ae1f0f295
EC2 Default User <ec2-user@ip-10-0-82-178.ap-northeast-1.compute.internal>
2021-07-10 22:51:11 +0000

    add code for amplify-auth

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,18 @@
 <template>
-  <router-view/>
+  <div>
+    <div v-if="isSignedIn">
+        <amplify-greetings></amplify-greetings>
+    </div>
+    <router-view/>
+  </div>
 </template>
+
+<script>
+    export default {
+        computed: {
+            isSignedIn() {
+                return this.$store.state.user !== null
+            },
+        },
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import store from "@/store/index.js"
+import Auth from "@aws-amplify/auth"
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components"
 import SignIn from "../views/SignIn.vue"
 import AlbumIndex from "../views/album/Index.vue"
 import AlbumCreate from "../views/album/Create.vue"
@@ -16,36 +19,41 @@ const routes = [
   },
   {
     path: "/Signin",
-    name: "SingIn",
+    name: "SignIn",
     component: SignIn,
   },
   {
     path: "/albums",
     name: "AlbumIndex",
-    component: AlbumIndex
+    component: AlbumIndex,
+    meta: { requireAuth: true },
   },
   {
     path: "/albums/create",
     name: "AlbumCreate",
     component: AlbumCreate,
+    meta: { requireAuth: true },
   },
   {
     path: "/albums/:albumId/edit",
     name: "AlbumEdit",
     component: AlbumEdit,
     props: true,
+    meta: { requireAuth: true },
   },
   {
     path: "/albums/:albumId",
     name: "AlbumSHow",
     component: AlbumShow,
     props: true,
+    meta: { requireAuth: true },
   },
   {
     path: "/albums/:albumId/photo/create",
     name: "PhotoCreate",
     component: PhotoCreate,
     props: true,
+    meta: { requireAuth: true },
   }
 ]
 
@@ -55,4 +63,40 @@ const router = new VueRouter({
   routes
 })
 
+function getAuthenticatedUser() {
+  return Auth.currentAuthenticatedUser()
+    .then((data) => {
+      if(data && data.signInUserSession) {
+        store.commit("setUser", data)
+        return data
+      }
+    })
+    .catch((e) => {
+      console.error(e)
+      store.commit("setUser", null)
+      return null
+    })
+}
+
+let user
+router.beforeResolve(async (to, from, next) => {
+  user = await getAuthenticatedUser()
+  
+  if (to.name === "SignIn" && user){
+    return next({ name: "AlbumIndex"})
+  }
+  if (to.matched.some((record) => record.meta.requireAuth) && !user){
+    return next({ name: "SignIn"})
+  }
+  return next()
+})
+
+onAuthUIStateChange((authState, authData) => {
+  if(authState === AuthState.SignedIn && authData){
+    router.push({ name: "AlbumIndex"})
+  }
+  if(authState === AuthState.SignedOut){
+    router.push({name: "SignIn"})
+  }
+})
 export default router
